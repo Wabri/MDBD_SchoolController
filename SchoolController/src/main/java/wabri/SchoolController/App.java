cb140fb9f5dc94b27e8167f87609bb97257bc406
Wabri <gabriele.puliti@gmail.com>
2017-11-20 16:24:59 +0100

    Created pom with general configuration for TDD

---
+++ SchoolController/src/main/java/wabri/SchoolController/App.java
@@ -0,0 +1,13 @@
+package wabri.SchoolController;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
